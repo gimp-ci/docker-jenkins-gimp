2676d1e1d4c8f99fe6bd4c7775338dac799db4fa
Sam Gleske <sam.mxracer@gmail.com>
2018-05-19 10:42:13 -0700

    gimp.sh build script

---
+++ debian-testing/gimp.sh
@@ -0,0 +1,26 @@
+set -xeo pipefail
+
+initial_workspace="$PWD"
+
+# dependencies
+pushd "$PREFIX"
+for x in /data/{babl,gegl,libmypaint,mypaint-brushes}-internal.tar.gz; do
+    tar -xzf "${x}"
+done
+popd
+
+# build
+PRODUCT=gimp
+git clone --reference /export/"${PRODUCT}".git git://git.gnome.org/"${PRODUCT}"
+cd "${PRODUCT}"/
+./autogen.sh
+./configure --prefix="$PREFIX"
+
+make "-j$(nproc)" install
+## package binaries for use in GIMP build
+#pushd "$PREFIX"
+#tar -czvf ~1/"${PRODUCT}"-internal.tar.gz lib/*"${PRODUCT}"* include/"${PRODUCT}"* lib/pkgconfig/"${PRODUCT}"* bin share
+#popd
+#cp -f "${PRODUCT}"-internal.tar.gz /data/
+
+#cd "${initial_workspace}"
