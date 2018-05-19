5dacfef858f0d4821abfa44a9d58e24c3be66a4c
Sam Gleske <sam.mxracer@gmail.com>
2018-05-18 23:26:57 -0700

    Update GEGL build script

--- debian-testing/gegl.sh
+++ debian-testing/gegl.sh
@@ -1,13 +1,11 @@
 set -xeo pipefail
-if [ -f /data/babl-internal.tar.gz ]; then
-    (
-    cd "$PREFIX"
-    tar -xzf /data/babl-internal.tar.gz
-    )
-else
-    echo 'ERROR: must run BABL build first.' >&2
-    exit 1
-fi
+
+# dependencies
+pushd "$PREFIX"
+tar -xzf /data/babl-internal.tar.gz
+popd
+
+# build
 PRODUCT=gegl
 git clone --reference /export/"${PRODUCT}".git git://git.gnome.org/"${PRODUCT}"
 cd "${PRODUCT}"/
